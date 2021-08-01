207cf37ff1d72caa39f489488db7a53475e0cf41
冰森 <dingheng.hdh@alibaba-inc.com>
2021-08-01 16:16:53 +0800

    feat: http -> https 重定向

--- codec/site/index.js
+++ codec/site/index.js
@@ -2,7 +2,15 @@ var getRawBody = require('raw-body');
 
 exports.handler = (req, resp, context) => {
   getRawBody(req, function(err, body) {
+    // 判断当前请求是否基于 http 协议
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      // 是的话就重定向到 https 协议
+      resp.headers['Location'] = 'https://codec.run';
+      resp.statusCode = 302;
+      resp.send('302 Moved Temporarily');
+      return;
+    }
     resp.headers['content-type'] = 'text/html';
-    resp.send('hello world, bilibili???');
+    resp.send('hello https!');
   }); 
 }
